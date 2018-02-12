633e9464a18a22497edb2ac52c89df0d6f97eff4
王俊超 <shining-glory@qq.com>
2018-02-12 23:20:44 +0800

    commit

---
+++ 005-使用zookeeper/src/main/java/wjc/zookeeper/ZooKeeper_Constructor_Usage_Simple.java
@@ -0,0 +1,45 @@
+package wjc.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ * Author: 王俊超
+ * Date: 2018-02-12 23:18
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+//Chapter: 5.3.1 Java API -> 创建连接 -> 创建一个最基本的ZooKeeper对象实例
+public class ZooKeeper_Constructor_Usage_Simple implements Watcher {
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws Exception {
+
+        ZooKeeper zookeeper = new ZooKeeper(
+                "domain1.book.zookeeper:2181",
+                5000, //
+                new ZooKeeper_Constructor_Usage_Simple());
+        System.out.println(zookeeper.getState());
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ZooKeeper session established.");
+    }
+
+    public void process(WatchedEvent event) {
+        System.out.println("Receive watched event：" + event);
+        if (Event.KeeperState.SyncConnected == event.getState()) {
+            connectedSemaphore.countDown();
+        }
+    }
+}
