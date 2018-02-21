b0d948691d8292cbb3a5ebbf1a963f859d140477
王俊超 <shining-glory@qq.com>
2018-02-21 18:50:48 +0800

    commit

---
+++ 005-使用zookeeper/src/main/java/wjc/zookeeper/$5_3_4/GetData_API_Sync_Usage.java
@@ -0,0 +1,57 @@
+package wjc.zookeeper.$5_3_4;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * <pre>
+ * ZooKeeper API 获取节点数据内容，使用同步(sync)接口。
+ * </pre>
+ * Author: 王俊超
+ * Date: 2018-02-21 18:44
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class GetData_API_Sync_Usage implements Watcher {
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+    private static ZooKeeper zk = null;
+    private static Stat stat = new Stat();
+
+    public static void main(String[] args) throws Exception {
+
+        String path = "/zk-book-" + System.currentTimeMillis();
+        zk = new ZooKeeper("localhost:2181",
+                5000, //
+                new GetData_API_Sync_Usage());
+        connectedSemaphore.await();
+        zk.create(path, "123".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+        System.out.println(new String(zk.getData(path, true, stat)));
+        System.out.println(stat.getCzxid() + "," + stat.getMzxid() + "," + stat.getVersion());
+
+        zk.setData(path, "123".getBytes(), -1);
+
+//        Thread.sleep(Integer.MAX_VALUE);
+        zk.close();
+    }
+
+    public void process(WatchedEvent event) {
+        if (Event.KeeperState.SyncConnected == event.getState()) {
+            if (Event.EventType.None == event.getType() && null == event.getPath()) {
+                connectedSemaphore.countDown();
+            } else if (event.getType() == Event.EventType.NodeDataChanged) {
+                try {
+                    System.out.println("-->" + new String(zk.getData(event.getPath(), true, stat)));
+                    System.out.println("-->" + stat.getCzxid() + "," +
+                            stat.getMzxid() + "," +
+                            stat.getVersion());
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+}
